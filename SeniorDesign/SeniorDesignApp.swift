6c409e797b438f17d2511abf897d3fb7f0798e8a
MetaJT <jordantrotter3@gmail.com>
2023-10-03 13:47:03 -0500

    Initial Commit

---
+++ SeniorDesign/SeniorDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorDesignApp.swift
+//  SeniorDesign
+//
+//  Created by Jordan Trotter on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SeniorDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
